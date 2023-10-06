dda76c4ed61fe3a31337ff3799142818c1b52fb8
Fausto G. Cintra <professor@faustocintra.com.br>
2023-10-06 12:38:06 +0000

    ..

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import Exercicio01 from './exercicios/01'
+import Exercicio02 from './exercicios/02'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <ul style={{ listStyleType: 'none' /* Lista sem marcadores */}}>
           <li> <Link to="/">Página Inicial</Link> </li>
           <li> <Link to="/01">Exercício 01</Link> </li>
+          <li> <Link to="/02">Exercício 02</Link> </li>
         </ul>
 
         <hr />
@@ -21,6 +23,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/01" element={<Exercicio01 />} />
+            <Route path="/02" element={<Exercicio02 />} />
           </Routes>
         </div>
 
